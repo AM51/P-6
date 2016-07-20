c68fe64894db0c9da428f608e973709f34bc45d5
archit mittal <archit.m@Archit-Mittal.local>
2016-07-20 09:34:00 +0530

    checklist changes for wearable

--- wearables/src/main/java/com/example/android/sunshine/app/CustomWatchFaceService.java
+++ wearables/src/main/java/com/example/android/sunshine/app/CustomWatchFaceService.java
@@ -1,4 +1,4 @@
-package com.example.wearables;
+package com.example.android.sunshine.app;
 
 import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
--- wearables/src/main/java/com/example/android/sunshine/app/MyWatchFace.java
+++ wearables/src/main/java/com/example/android/sunshine/app/MyWatchFace.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.wearables;
+package com.example.android.sunshine.app;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -32,6 +32,7 @@ import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
 import android.text.format.Time;
 import android.view.SurfaceHolder;
+
 
 import java.lang.ref.WeakReference;
 import java.util.TimeZone;
